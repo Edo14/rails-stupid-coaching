c0d5a36da9475f18f3b363e20e731a0fafb4a682
Edoardo Achenza <edoardoachenza@gmail.com>
2021-03-01 15:52:25 +0100

    Finished second excercise

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,6 +7,10 @@ class QuestionsController < ApplicationController
 
     if @question == "I am going to work"
       @answer = "Great!"
+    elsif @question == "Hello there"
+      @answer = "I don't care, get dressed and go to work!"
+    else @question == "Can I go shopping?"
+      @answer = "Silly question, get dressed and go to work!"
     end
   end
 end
